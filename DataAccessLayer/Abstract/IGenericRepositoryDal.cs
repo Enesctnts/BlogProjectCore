c590e7b3c005b3dedc48d722c94d6ac429e677ad
Enes Çetintaş <enesctnts@gmail.com>
2022-05-13 01:34:51 +0300

    Veri tabanı CRUD işlemlerinin yazıldıgı interfaceler oluşturuldu.

---
+++ DataAccessLayer/Abstract/IGenericRepositoryDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericRepositoryDal<T> where T:class
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        List<T> GetlistAll(T entity);
+        T GetByID(int id);
+
+    }
+}
