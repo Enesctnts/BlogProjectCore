2d7cb13e2542065b27ba02cdb4af8579d9ecb4f1
Enes Çetintaş <enesctnts@gmail.com>
2022-05-13 00:08:40 +0300

    Veri tabanı tablolarına denk gelen sınıflar oluşturulmaya başlandı. Contact sınıfı oluşturuldu.

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Contact
+    {
+        //İletişim
+
+        [Key]
+        public int ContactId { get; set; }
+        public string ContactUserName { get; set; }
+        public string ContactMail { get; set; }
+        public string ContactSubject { get; set; }
+        public string ContactMessages { get; set; }
+        public DateTime ContactDate { get; set; }
+        public bool ContactStatus { get; set; }
+
+    }
+}
