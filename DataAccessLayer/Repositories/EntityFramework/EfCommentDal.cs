86353b5014d62ae80793a40b695a7a89574a3039
Enes Çetintaş <enesctnts@gmail.com>
2022-05-13 01:36:52 +0300

    Veri tabanı CRUD işlemlerini yapabilmek için somut sınıflar yazıldı.

---
+++ DataAccessLayer/Repositories/EntityFramework/EfCommentDal.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.EntityFramework
+{
+    public class EfCommentDal : GenericRepositoryDal<Comment>, ICommentDal
+    {
+    }
+}
