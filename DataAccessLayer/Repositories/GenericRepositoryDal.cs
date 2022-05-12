d5d177ec67b1a1ba14d43d67bf13099b11c3de37
Enes Çetintaş <enesctnts@gmail.com>
2022-05-13 01:39:41 +0300

    CRUD işlemleri yapılabilmesi için GenericRepositoryDal oluşturuldu. Bu sınıf sayesinde aynı işi yapan sınıfların hepsine aynı kodları yazmak zorunda kalmıcaz. Bu sınıftan miras alarak bütün sınıflar crud işlemlerini yapabilecek.

---
+++ DataAccessLayer/Repositories/GenericRepositoryDal.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepositoryDal<T> : IGenericRepositoryDal<T> where T : class
+    {
+        public void Delete(T entity)
+        {
+            //using ile tanımlandıgı zaman IDisposable metodu sayesinde garbage collector'ü beklemeden kendisi bellekten siler.
+            using var context = new Context();
+            context.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetlistAll(T entity)
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T entity)
+        {
+            using var context = new Context();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            using var context = new Context();
+            context.Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
