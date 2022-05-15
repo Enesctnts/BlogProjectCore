67145f3e4163a7fe1b3e10544d52639406d083b1
Enes Çetintaş <enesctnts@gmail.com>
2022-05-15 16:52:55 +0300

    Yorum ekleme ve listeleme işlemleri yapıldı. Bu işlemler partial olarak yapıldı. Partial yapmamızın sebebi her tarafta kullanabilecek olmamız ve daha kolay kontrol edebilmek için.

---
+++ PresentationLayer/Controllers/CommentController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Yorum ekleme
+        public PartialViewResult PartialAddComment()
+        {
+            return PartialView();
+        }
+
+        //Bloglardaki yorum listesi
+        public PartialViewResult CommentListByBlog(int id)
+        {
+            var values = commentManager.GetAll(id);
+            return PartialView(values);
+        }
+    }
+}
