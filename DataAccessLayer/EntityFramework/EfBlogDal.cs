31fb012acf06edbde510117a32c5b5761db53106
Enes Çetintaş <enesctnts@gmail.com>
2022-05-15 16:46:36 +0300

    Blog içinde include işlemi yaptık. Blogları çagırırken category adını çagırınca hata veriyor. Bu yüzden include işlemi yaptık. Bu metodda include işlemi yapılan metodu çagırcaz.

--- DataAccessLayer/EntityFramework/EfBlogDal.cs
+++ DataAccessLayer/EntityFramework/EfBlogDal.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,14 @@ namespace DataAccessLayer.EntityFramework
 {
     public class EfBlogDal : GenericRepositoryDal<Blog>, IBlogDal
     {
+        //Burayı yapmamızın amacı Blogları listelerken Category Adını alırken sistem hata veriyor. Bunun önüne geçmek için Hangi Entityde iş yapıyorsakonun interfacesinde ve Somut sınıf tarafında bu işlemleri yapmak gerekiyor. Include işlemi ilişkili oldugun sınıflarıda getir demek.Joın işlemi gibi. Bu işleme Eager Loading deniyor
+
+        public List<Blog> GetListWithCategory()
+        {
+            using(var context = new Context())
+            {
+                return context.Blogs.Include(x=>x.Category).ToList();
+            };
+        }
     }
 }
