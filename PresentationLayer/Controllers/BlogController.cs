c9d8045e4dbf2ec3c97a45198d3351c07ae2bc66
Enes Çetintaş <enesctnts@gmail.com>
2022-05-15 16:51:42 +0300

    Blokların sayfada listelenme işlemi yapıldı. Blok detayları sayfası işlemleri yapıldı.

--- PresentationLayer/Controllers/BlogController.cs
+++ PresentationLayer/Controllers/BlogController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,16 @@ namespace PresentationLayer.Controllers
 {
     public class BlogController : Controller
     {
+        BlogManager blogManager = new BlogManager(new EfBlogDal());
         public IActionResult Index()
         {
+            var values = blogManager.GetBlogListWithCategory();
+            return View(values);
+        }
+
+        public IActionResult BlogReadAll(int id)
+        {
+            var values = blogManager.GetBlogById(id);
             return View();
         }
     }
